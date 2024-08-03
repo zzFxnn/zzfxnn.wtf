a15a87dc528ab91207f1768da5e766b087d23a02
zzFxnn <113517275+zzFxnn@users.noreply.github.com>
2024-08-03 05:09:06 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
 import Loadingbar from "@/components/Loadingbar";
 import BottomWaves from "@/components/BottomWaves";
 import TopWaves from "@/components/TopWaves";
 import ContextMenu from "@/components/ContextMenu";
 
 const App = ({ Component, pageProps }: AppProps) => {
-    useEffect(() => {
-        const announcer = document.getElementById("__next-route-announcer__");
-
-        if (announcer) {
-            announcer.remove();
-        }
-    }, []);
-
     return (
         <>
             <main className="min-h-screen w-screen flex flex-col items-center justify-center overflow-hidden bg-slate-900 selection:text-blue-600 selection:bg-gray-950/25 pattern-grid-lg text-gray-800/50">
